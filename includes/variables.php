9751553e4ab91a1e2278d5805a98bb3f7f234989
iberlot <ivanberlot@gmail.com>
2016-11-15 07:53:48 -0300

    desde eclipse

---
+++ includes/variables.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * 
+ * @author iberlot <@> ivanberlot@gmail.com
+ * @todo FechaC 14/11/2016 - Lenguaje PHP 
+ * 
+ * @todo variables.php
+ *
+ * @todo Inicializacion de las variables usadas por el sistema
+ * @version 0.1 - Version de inicio 
+ * @package CMS/includes
+ * @category Basics
+ */
+
+/*
+ * Querido programador:
+ *
+ * Cuando escribi este codigo, solo Dios y yo sabiamos como funcionaba.
+ * Ahora, Solo Dios lo sabe!!!
+ *
+ * Asi que, si esta tratando de 'optimizar' esta rutina y fracasa (seguramente),
+ * por favor, incremente el siguiente contador como una advertencia para el
+ * siguiente colega:
+ *
+ * totalHorasPerdidasAqui = 1
+ *
+ */
+
+/**
+ * Nombre de asociado a la cuenta del administrador
+ */
+$nombre = '';
+
+
+$usuario = '';
+$contrasena = '';
+$email = '';
+
+$mensaje = '';
+?>
